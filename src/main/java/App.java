d23b7d80311a533e41078195f3394788c139cf36
Kariuki62 <kariukiwan62@gmail.com>
2019-07-04 07:28:08 -0400

    Added the method for Heroku deployment.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,8 +7,19 @@ import java.util.Map;
 
 import static spark.Spark.*;
 
+//    public static void main(String[] args){
+//        staticFileLocation("/public");
 public class App {
-    public static void main(String[] args){
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+    public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
 
         get("/", (request, response) -> {
